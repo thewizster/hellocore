de47e680fd542c75e83fe16fadb07f4fbc8932d0
Raymond Brady <raymond@karservices.com>
2017-10-27 19:20:21 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hellocore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Looking up Google.com...");
+            var addresses = System.Net.Dns.GetHostEntry("google.com").AddressList;
+            Console.WriteLine("The following addresses were found:");
+            foreach (var item in addresses)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine($"Local Host Name: {System.Net.Dns.GetHostName()}");
+            Console.ReadLine();
+        }
+    }
+}
